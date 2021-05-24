3aff0493c5ce388b412ed280c3a4ea057b282612
zjxht62 <zhao_ji_xiang@126.com>
2021-05-24 19:46:23 +0800

    feat:添加字符串格式相关代码

--- BeginningPython/chapter3/3.3设置字符串的格式_完整版.py
+++ BeginningPython/chapter3/3.3设置字符串的格式_完整版.py
@@ -6,5 +6,31 @@ full_name = ['trevor', 'zhao']
 print('Mr {name[1]}'.format(name=full_name))  # Mr zhao
 
 import math
+from math import pi
+
 tmpl = "The {mod.__name__} module defines the value {mod.pi} for π"
 print(tmpl.format(mod=math))
+
+# 指定转换标志
+print("{pi!s} {pi!r} {pi!a}".format(pi="π"))  # π 'π' '\u03c0'
+
+print("The number is {num}".format(num=42))
+# The number is 42
+print("The number is {num:f}".format(num=42))
+# The number is 42.000000
+print("The number is {num:b}".format(num=42))
+#  The number is 101010
+
+
+print("{num:10}".format(num=3))  #         3
+
+print("{name:10}".format(name="Trevor"))  #Trevor
+
+print("{pi:10.2f}".format(pi=pi))  #      3.14
+
+print( 'One googol is {:,}'.format(10**100))
+# One googol is 10,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000
+
+# 同时指定其他格式设置元素时，这个逗号应放在宽度和表示精度的句点之间①。
+print( 'One googol is {:10,.5f}'.format(10000))
+# One googol is 10,000.00000
