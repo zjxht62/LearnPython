9f4e4d7aeae43dcc2b964277e2b8cef86eac4b37
zjxht62 <zhao_ji_xiang@126.com>
2021-05-25 11:22:10 +0800

    feat:完成字符串格式化相关代码的整理

--- BeginningPython/chapter3/3.3设置字符串的格式_完整版.py
+++ BeginningPython/chapter3/3.3设置字符串的格式_完整版.py
@@ -22,15 +22,53 @@ print("The number is {num:b}".format(num=42))
 #  The number is 101010
 
 
-print("{num:10}".format(num=3))  #         3
+print("{num:10}".format(num=3))  # 3
 
-print("{name:10}".format(name="Trevor"))  #Trevor
+print("{name:10}".format(name="Trevor"))  # Trevor
 
-print("{pi:10.2f}".format(pi=pi))  #      3.14
+print("{pi:10.2f}".format(pi=pi))  # 3.14
 
-print( 'One googol is {:,}'.format(10**100))
+print('One googol is {:,}'.format(10 ** 100))
 # One googol is 10,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000
 
 # 同时指定其他格式设置元素时，这个逗号应放在宽度和表示精度的句点之间①。
-print( 'One googol is {:10,.5f}'.format(10000))
+print('One googol is {:10,.5f}'.format(10000))
 # One googol is 10,000.00000
+
+# 使用0来填充数字
+"{:010.2f}".format(pi)  # 0000003.14
+
+# 指定左对齐、右对齐和居中，可以分别使用<、>和^
+print("{0:<10.2f}\n{0:^10.2f}\n{0:>10.2f}".format(pi))
+# 3.14
+#   3.14
+#      3.14
+
+print("{:$^15}".format(" WIN BIG "))  # $$$ WIN BIG $$$
+print('{0:10.2f}\n{1:10.2f}'.format(pi, -pi))
+#      3.14
+#     -3.14
+print('{0:10.2f}\n{1:=10.2f}'.format(pi, -pi))
+#      3.14
+#-     3.14
+
+print("{0:-.2}\n{1:-.2}".format(pi, -pi))
+#3.1
+#-3.1
+print("{0:+.2}\n{1:+.2}".format(pi, -pi))
+#+3.1
+#-3.1
+
+#如果符号说明符指定为空格，那么会在正数前加上空格而不是`+`
+print("{0: .2}\n{1: .2}".format(pi, -pi))
+#3.1
+#-3.1
+
+
+# 对于二进制、八进制和十六进制转换，将加上一个前缀。
+print("{:b}".format(42)) # 101010
+print("{:#b}".format(42)) # 0b101010
+
+# 各种十进制数，它要求必须包含小数点
+print("{:g}".format(42)) # 42
+print("{:#g}".format(42)) # 42.0000
