7c17f60ff7e37f404ff4e45436391bfc3fed5bba
zjxht62 <zhao_ji_xiang@126.com>
2021-06-08 16:22:08 +0800

    feat

--- BeginningPython/chapter6/chapter6.py
+++ BeginningPython/chapter6/chapter6.py
@@ -1,16 +1,16 @@
 # 前10个斐波那契数列
 fib = [0, 1]
 for i in range(8):
-    fib.append(fib[-1] + fib[-2])
-print(fib)
+    fib.append(fib[-2] + fib[-1])
+print(fib)  # [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
 # 可以通过callable判断对象是否可调用
 import math
 
 x = 1
 y = math.sqrt
-print(callable(x))
-print(callable(y))
+print(callable(x))  # False
+print(callable(y))  # True
 
 
 # 定义函数
@@ -41,6 +41,23 @@ def haha():
 print(haha.__doc__)
 
 
+def test():
+    print('just a test')
+    return
+    print('another test')
+
+
+x = test()  # 打印出 just a test
+print(x)  # None
+
+list = [1, 2, 3]
+def replaceList(n):
+    n = [4, 5, 6]
+
+replaceList(list)
+print(list)  # [1, 2, 3]
+
+
 # 函数对于参数的改变
 def changeStr(s):
     s = "haha"
@@ -53,13 +70,12 @@ print(s1)
 
 
 # 但是如果是可变的数据结构就会被修改
+list = ['嘿嘿', '呵呵']
 def changeList(n):
     n[0] = '哈哈'
 
-
-l1 = [1, 2, 3]
-changeList(l1)
-print(l1)
+changeList(list)
+print(list)  # ['哈哈', '呵呵']
 
 # 举个例子来看函数的作用
 storage = {}
@@ -194,28 +210,38 @@ x = 1
 foo()  # foo并不会改变全局变量的值，
 print(x)
 
-#函数可以引用全局变量，但是可能导致bug，慎用
+# 函数可以引用全局变量，但是可能导致bug，慎用
 external = 'berry'
+
+
 def combine(parameter):
     print(parameter + external)
 
+
 combine("Shrub")
 
-#重新关联全局变量
+# 重新关联全局变量
 x = 2
+
+
 def changeGlobal():
-    #告诉Pythonx是全局变量
+    # 告诉Pythonx是全局变量
     global x
     x = x + 1
+
+
 changeGlobal()
 print(x)
+
 
 # 嵌套可以用一个函数创建另一个函数
 # 像multiplyByFactor这样存储其所在作用域的函数称为闭包。
 def multipilier(factor):
     def multiplyByFactor(number):
         return number * factor
+
     return multiplyByFactor
+
 
 double = multipilier(2)
 print(double(5))
@@ -223,13 +249,14 @@ print(double(5))
 triple = multipilier(3)
 print(triple(5))
 
-#6.6 递归
-#通常递归包含两个部分
+
+# 6.6 递归
+# 通常递归包含两个部分
 # 基线条件：满足这种条件时函数将直接返回一个值
 # 递归条件：包含一个或多个调用，这些调用旨在解决问题的一部分
 # 关键在于，将问题分解为较小的部分，可以避免递归没完没了，因为问题将被分解成基线条件可以解决的最小问题
 
-#递归实现阶乘
+# 递归实现阶乘
 # 1的阶乘为1。
 # 对于大于1的数字n，其阶乘为n  1的阶乘再乘以n。
 def factorial(n):
@@ -238,9 +265,11 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
+
 print(factorial(10))
 
-#二分查找
+
+# 二分查找
 def search(sequence, number, lower=0, upper=None):
     if upper is None: upper = len(sequence) - 1
     if lower == upper:
