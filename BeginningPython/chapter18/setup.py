a35df4c4672216f27101e305479664095ccd0e3f
zjxht62 <zhao_ji_xiang@126.com>
2021-08-02 21:45:01 +0800

    feat:添加完成18章，程序打包相关代码

--- BeginningPython/chapter18/setup.py
+++ BeginningPython/chapter18/setup.py
@@ -1,7 +1,17 @@
-from setuptools import setup
+# from setuptools import setup
+#
+# setup(name='Hello',
+#       version='1.0',
+#       description='A simple example',
+#       author='zjx',
+#       py_modules=['hello'])
 
-setup(name='Hello',
+
+from setuptools import setup, Extension
+
+setup(name='palindrome',
       version='1.0',
-      description='A simple example',
-      author='zjx',
-      py_modules=['hello'])
+      ext_modules=[
+          Extension('palindrome', ['palindrome2.c',
+                                   'palindrome2.i'])
+      ])
