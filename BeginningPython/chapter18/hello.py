50c7f3398220e794588a0fea0dbdfb9f23317314
zjxht62 <zhao_ji_xiang@126.com>
2021-08-02 18:56:30 +0800

    feat:添加程序打包相关内容

---
+++ BeginningPython/chapter18/hello.py
@@ -0,0 +1,2 @@
+def say_hello():
+    print('我是hello模块输出的内容')
---
+++ BeginningPython/chapter18/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Hello',
+      version='1.0',
+      description='A simple example',
+      author='zjx',
+      py_modules=['hello'])
