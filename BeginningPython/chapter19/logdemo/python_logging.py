2ac99cc1d7ab4e0ce4a27bb1246eff27e028cf94
zjxht62 <zhao_ji_xiang@126.com>
2021-08-10 17:37:49 +0800

    feat:添加19章相关内容

---
+++ BeginningPython/chapter19/logdemo/python_logging.py
@@ -0,0 +1,9 @@
+# 使用Python标准库模块logging
+import logging
+logging.basicConfig(level=logging.INFO, filename='mylog.log')
+logging.info('Starting program')
+logging.info('Trying to divide 1 by 0')
+print(1/0)
+logging.info('The division succeeded')
+logging.info('Ending program')
+logging.
