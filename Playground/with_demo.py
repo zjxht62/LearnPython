0659d81d53397f73aad877899822781f4cf77b70
ZhaoJixiang <zhaojixiang@zhnrh.com>
2023-01-16 11:37:52 +0800

    fet

---
+++ Playground/with_demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding:utf-8
+# with open('haha.txt', 'r') as f:
+#     raise Exception
+#     print(f.read())
+#
+
+
+
+#2021-12-22T((06)|(07)|(08)|(09)|(10)|(11)|(12)|(13)|(14)|(15)|(16)|(17)|(18)|(19)|(20))
+
+
+
+user_name_prefix = "presstest"
+for i in range(0,2000):
+    print(user_name_prefix + str(i), end='')
+    print(",Test_1234")
