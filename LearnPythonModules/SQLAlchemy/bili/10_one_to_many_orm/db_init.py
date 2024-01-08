1f353ed0dda9d72b7c9cda47f0d4887fdec41a8a
ZhaoJixiang <zhaojixiang@zhnrh.com>
2024-01-08 11:04:36 +0800

    [feat]添加BiliBili中SQLAlchemy课程中ORM一对多插入和查询

--- LearnPythonModules/SQLAlchemy/bili/10_one_to_many_orm/db_init.py
+++ LearnPythonModules/SQLAlchemy/bili/10_one_to_many_orm/db_init.py
@@ -1,8 +1,9 @@
 import datetime
+from typing import List
 
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 # from sqlalchemy.ext.declarative import declarative_base  # 用于映射类的基类
-from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, declarative_base  # 用于映射类的基类
+from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, declarative_base, relationship  # 用于映射类的基类
 from typing_extensions import Annotated
 from sqlalchemy.sql import func
 
@@ -20,7 +21,8 @@ class Department(Base):
 
     id: Mapped[int_pk]
     name: Mapped[required_unique_name]
-
+    # 不建议在里面定义一对多中多的关联
+    employees: Mapped[List["Employee"]] = relationship(back_populates="department")
     def __repr__(self):
         return f'id: {self.id}, name: {self.name}'
 
@@ -32,6 +34,12 @@ class Employee(Base):
     dep_id: Mapped[int] = mapped_column(ForeignKey('department.id'))
     name: Mapped[required_unique_name]
     birthday: Mapped[timestamp_not_null]
+
+    # 引入关联的数据表对象,不是一个真正的数据库字段
+    # 通过relationship的lazy属性，可控制关联对象的查询行为，为True时，会在需要时才查询，默认为True
+    # 使用backref，可以动态给department增加一个employees属性,实现双向关联，但是在Department中没有显示
+    # 使用back_populates，需要在department中定义
+    department:Mapped[Department] = relationship(lazy=False, back_populates='employees')
 
     def __repr__(self):
         return f'id: {self.id}, dep_id: {self.dep_id}, name: {self.name}, birthday: {self.birthday}'
--- LearnPythonModules/SQLAlchemy/bili/10_one_to_many_orm/query.py
+++ LearnPythonModules/SQLAlchemy/bili/10_one_to_many_orm/query.py
@@ -2,14 +2,36 @@ from db_init import Session, Department, Employee
 
 
 def insert_employee(session):
-    d1 = Department(name='hr')
-    session.add(d1)
+    # 方案1：插入部门记录后，执行flush，获取到id，之后再插入员工信息
+    # d1 = Department(name='hr')
+    # session.add(d1)
+    # 通过flush，将add操作提交
+    # 但是这不是好的解决方案
+    # session.flush()
+    # e1 = Employee(dep_id=d1.id, name='Jack', birthday='2000-10-1')
+    # session.add(e1)
 
-    e1 = Employee(dep_id=d1.id, name='Jack', birthday='2000-10-1')
+    # 方案2：定义关联表对象
+    d1 = Department(name='hr')
+    e1 = Employee(department=d1, name='Jack', birthday="2000-10-1")
     session.add(e1)
 
     session.commit()
 
 
+def select_employee(session):
+    emp = session.query(Employee).filter(Employee.id == 1).one()
+
+    print(emp)
+    print(emp.department)
+
+def select_department(session):
+    dep = session.query(Department).filter(Department.id == 1).one()
+
+    print(dep)
+    print(dep.employees)
+
 session = Session()
-insert_employee(session)
+# insert_employee(session)
+# select_employee(session)
+select_department(session)
